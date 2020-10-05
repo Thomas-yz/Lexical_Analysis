d7ac87919b815f19907534ef55d0fd20e377dda4
Thomas-yz <63227642+Thomas-yz@users.noreply.github.com>
2020-10-05 18:20:42 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Test {
 	public static void main(String args[]) throws IOException{
-		Lexical.AnalyzeProgram("src/hello.txt", "src/output.txt");
+		Lexical.AnalyzeProgram(args[0]);
 		return;
 	}
 }
